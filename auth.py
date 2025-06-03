f96e6fbf68247133759bfb018d229373e3b2771a
ihsaan-kt <ishaan.tam@proton.me>
2025-06-03 13:38:37 +0800

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,4 +1,3 @@
-# auth.py
 from typing import Optional, Dict
 from datetime import datetime, timedelta
 
@@ -55,6 +54,7 @@ mock_users = {
     },
 }
 
+
 # Password
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
@@ -79,13 +79,15 @@ def authenticate_user(username: str, password: str) -> Optional[UserInDB]:
 # JWT Token
 def create_access_token(data: Dict, expires_delta: Optional[timedelta] = None) -> str:
     to_encode = data.copy()
+    
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
         expire = datetime.utcnow() + timedelta(minutes=15)
+        
     to_encode.update({"exp": expire})
     # encoding PyJWT encoding
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, ALGORITHM) # No 'algorithm=' needed
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, ALGORITHM)
     return encoded_jwt
 
 
@@ -95,9 +97,11 @@ async def get_current_user(token: str = Depends(oauth2_scheme)) -> User:
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
+    
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
+        
         if username is None:
             raise credentials_exception
         token_data = TokenData(username=username)
